b4e6d7e63eccc3ccdad10a34b7bd01e6172eaf6a
ty <1206036143@qq.com>
2024-07-16 21:58:54 +0800

    初始化工程

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 尚硅谷视频硅谷甄选学习
 
 视频地址：https://www.bilibili.com/video/BV1Xh411V7b5
+
+
+15
---
+++ admin_project/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+
+
+<h1>小小樱</h1>
+</template>
+
+
+
+
+<script setup lang="ts">
+
+
+
+</script>
+
+
+
+
+<style scoped>
+
+
+</style>
