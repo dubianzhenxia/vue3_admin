dbc60f8ba7bf35184a7cdd7af8830017ea50fbb3
ty <1206036143@qq.com>
2024-07-17 22:26:44 +0800

    引入element-plus,并进行相关配置

--- admin_project/src/App.vue
+++ admin_project/src/App.vue
@@ -1,7 +1,19 @@
 <template>
-
-
-<h1>小小樱</h1>
+<el-button type="primary" :icon="Search">小小樱</el-button> 
+<el-rate v-model="value" size="large" />
+<el-icon><Search /></el-icon>
+<!-- <el-pagination
+      v-model:current-page="currentPage4"
+      v-model:page-size="pageSize4"
+      :page-sizes="[100, 200, 300, 400]"
+      :size="size"
+      :disabled="disabled"
+      :background="background"
+      layout="total, sizes, prev, pager, next, jumper"
+      :total="400"
+      @size-change="handleSizeChange"
+      @current-change="handleCurrentChange"
+    /> -->
 </template>
 
 
@@ -9,7 +21,10 @@
 
 <script setup lang="ts">
 
+import { ref } from 'vue'
+import {Search} from '@element-plus/icons-vue'
 
+const value = ref(0)
 
 </script>
 
--- admin_project/src/main.ts
+++ admin_project/src/main.ts
@@ -1,4 +1,29 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+//引入element-plus
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
+//配置element-plus国际化
+import zhCn from 'element-plus/es/locale/lang/zh-cn'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+app.use(ElementPlus, {
+    locale: zhCn,//设置element-plus使用中文
+  })
+  
+
+  /**
+   * 注册图标组件为全局组件，注意这里的组件是以组件标签的形式使用如 <Search />
+   * 就是说例如button :icon='Search' 这里面的Search组件使用时需要在vue文件中再次引入，注册
+   * 如：
+   * 先 import {Search} from '@element-plus/icons-vue'
+   * 再 <el-button type="primary" :icon="Search">小小樱</el-button>
+   * 或者写成字符串如
+   *   el-button type="primary" icon="Search">小小樱</el-button> //无需引入注册组件，icon不使用:（v-bind）
+   */
+  for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+    app.component(key, component)
+  }
+app.use(ElementPlus)
+app.mount('#app')
